6d4ee2520b44895da89fd35bd3d15ebe8c183438
AtharvaParwal <atharvaparwal0608@gmail.com>
2025-03-21 11:39:52 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,8 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
